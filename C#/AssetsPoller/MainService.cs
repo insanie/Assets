38f2aad15d28f2f4f8d0d917b0521a3f242438a2
insanie <insanie@protonmail.com>
2018-10-15 17:46:11 +0300

    Poller service | Beautiful birth
    Assets poller service officially born as a useless tets service that does nothing but spamming in event log every minute

---
+++ C#/AssetsPoller/MainService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace AssetsPoller
+{
+	public partial class MainService : ServiceBase
+	{
+		private Int32 eventId = 1;
+
+		public MainService()
+		{
+			InitializeComponent();
+
+			assetsEventLog = new EventLog();
+			if (!EventLog.SourceExists("AssetsServiceSource"))
+			{
+				EventLog.CreateEventSource("AssetsServiceSource", "AssetsServiceLog");
+			}
+			assetsEventLog.Source = "AssetsServiceSource";
+			assetsEventLog.Log = "AssetsServiceLog";
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			assetsEventLog.WriteEntry("Assets service started");
+			// Set up a timer that triggers every minute.
+			System.Timers.Timer timer = new System.Timers.Timer();
+			timer.Interval = 60000; // 60 seconds
+			timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+			timer.Start();
+		}
+
+		protected override void OnStop()
+		{
+			assetsEventLog.WriteEntry("Assets service stopped");
+		}
+
+		public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+		{
+			// TODO: Insert monitoring activities here.
+			assetsEventLog.WriteEntry("Assets timer elapsed", EventLogEntryType.Information, eventId++);
+		}
+	}
+}
--- C#/AssetsPoller/Program.cs
+++ C#/AssetsPoller/Program.cs
@@ -16,7 +16,7 @@ namespace AssetsPoller
 			ServiceBase[] ServicesToRun;
 			ServicesToRun = new ServiceBase[]
 			{
-				new Service1()
+				new MainService()
 			};
 			ServiceBase.Run(ServicesToRun);
 		}
